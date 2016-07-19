6c45ee356cd676499ad8c842496f4693f2e5f734
Mathieu Dufour <mathieu_dufour@hotmail.fr>
2016-07-19 13:50:19 +0200

    compteur + modulo 3, 5 and 3 + 5

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    for ( var i = 0; i < 101; i++) {
+        if ( (i % 3) == 0 && (i % 5) == 0)
+            res.write('invivox\n');
+        else if ( (i % 3) == 0 )
+            res.write('invi\n');
+        else if ( (i % 5) == 0 )
+            res.write('vox\n');
+        else res.write(i + '\n');
+    }
+    res.end('END...\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
